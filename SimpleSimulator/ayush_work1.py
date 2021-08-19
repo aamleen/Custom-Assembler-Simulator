af9b90b4be8e442a4cb14ec9452d48ef5d9541e2
Ayush_Singal <ayush20365@iiitd.ac.in>
2021-08-19 22:41:22 +0530

    added some methods

---
+++ SimpleSimulator/ayush_work1.py
@@ -0,0 +1,31 @@
+from dicts_and_opes import * 
+
+def add(line , pc): # add R1 R2 R3   [type] op[5bit] [2unused] reg[3bit] reg[3bit] reg[3bit]}
+    op = line[:5]
+    reg_str_add  = line[7:10]              #storing the destination register 
+    reg1_str_add  = line[10:13]             # storing these two regis whose data is going to be manipulated
+    reg2_str_add  = line[13:]
+    reg_sum = registers[reg1_str_add] + registers[reg2_str_add]
+
+
+    registers[reg_str_add] =reg_sum
+    aFlag = None
+
+
+def sub(line , pc): # add R1 R2 R3   [type] op[5bit] [2unused] reg[3bit] reg[3bit] reg[3bit]}
+    op = line[:5]
+    reg_str_add  = line[7:10]
+    reg1_str_add  = line[10:13]
+    reg2_str_add  = line[13:]
+    registers[reg_str_add] = registers[reg1_str_add] - registers[reg2_str_add]
+
+def mul(line , pc): # add R1 R2 R3   [type] op[5bit] [2unused] reg[3bit] reg[3bit] reg[3bit]}
+    op = line[:5]
+    reg_str_add  = line[7:10]
+    reg1_str_add  = line[10:13]
+    reg2_str_add  = line[13:]
+    registers[reg_str_add] = registers[reg1_str_add]*registers[reg2_str_add]
+
+
+
+    
